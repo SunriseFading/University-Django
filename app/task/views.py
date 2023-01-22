0e6bbc02754324c1f150ef0090f2902a4d16c7c8
SunriseFading <sunrisefading@proton.me>
2023-01-22 19:49:55 +0300

    Complete project

--- app/task/views.py
+++ app/task/views.py
@@ -22,7 +22,9 @@ class ReportRetrievalView(APIView):
     def get(self, request, task_id):
         task = AsyncResult(task_id)
         if task.state != "SUCCESS":
-            return Response(data={"status": task.state}, status=status.HTTP_200_OK)
+            return Response(
+                data={"status": task.state}, status=status.HTTP_102_PROCESSING
+            )
         report = open(f"media/{task.result}", "rb")
         response = HttpResponse(
             content=report,
